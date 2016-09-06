c84bf8001f5533d941937062cc0a881beabf7453
Calle Robertsson <calle@robcon.se>
2016-09-06 08:52:45 +0200

    Improve go eye script

--- eye-go.sh
+++ eye-go.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
+# eye script to be executed on file changes
+# I.e. `eye -r -m '\.go$ -c './eye-dev.sh'`
+# See: https://github.com/callerobertsson/eye
 
+# Execute command
+# First arg is a label, the rest will be executed
 function execOrDie {
-    echo text $0
-    local args=$@
-    echo cmd $@:1
-    local status=$?
-    if [ $status -ne 0 ]; then
-        echo "error with $1" >&2
+    echo "${@:1:1}..."
+    res=$((${@:2}) 2>&1) # redir stderr to stdout and store in res
+
+    if [ $? -ne 0 ]; then
+        echo ""
+        echo "---> ERROR!"
+        echo "Command: $1" 
+        echo "Output"
+        echo "$res"
         exit
     fi
-    return $status
 }
 
-echo directory: `pwd`
+# Steps to execute
 execOrDie "Go Build" go build -o /dev/null
 execOrDie "Go Lint" golint
 execOrDie "Go Vet" go vet
-echo Done
+
+echo "...done"
 
